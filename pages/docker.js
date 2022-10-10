d9513044c3c9a772e24d0c10d38e8d9bcd57a1e1
Mertcan Kose <mertcankose97@outlook.com>
2022-10-10 18:59:18 +0300

    docker-first

--- pages/docker.js
+++ pages/docker.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Docker() {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +13,10 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Happy Birthday 🥳{" "}
+          <a href="https://docker.com" target="_blank" rel="noreferrer">
+            Docker!
+          </a>
         </h1>
 
         <p className={styles.description}>
